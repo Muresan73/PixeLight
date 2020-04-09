47e22fa55198b2d8e9411dc2f5ca492c5788b3d5
Gergely Módi <gmodi@chemaxon.com>
2020-04-09 14:30:15 +0200

    watch build

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
-var sourcemaps = require('gulp-sourcemaps');
-
+const sourcemaps = require('gulp-sourcemaps');
+const runElectron = require('gulp-run-electron');
 
 gulp.task('typescript', function () {
   const tsProject = ts.createProject('tsconfig.json');
-
-  var tsResult =  tsProject.src().pipe(sourcemaps.init()).pipe(tsProject());
-
+  const tsResult = tsProject.src().pipe(sourcemaps.init()).pipe(tsProject());
   return tsResult.js.pipe(sourcemaps.write()).pipe(gulp.dest('dist'));
 });
-gulp.task('html', function() {
-  return gulp.src(['src/index.html'])
-    .pipe(gulp.dest('dist'));
+
+gulp.task('html', function () {
+  return gulp.src(['src/index.html']).pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulp.series('typescript', 'html'));
+gulp.task('watcher', function () {
+  gulp.watch('src/**/*.ts', gulp.series('typescript', runElectron.rerun));
+  gulp.watch('src/index.html', gulp.series('html'));
+});
+
+gulp.task('electron', function () {
+  return gulp.src('dist').pipe(runElectron(['dist/main.js']));
+});
+
+gulp.task('default', gulp.series('typescript', 'html', 'electron'));
+
+gulp.task('watch', gulp.series('default', 'watcher'));
